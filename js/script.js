91f440459d73239a47a5eb3fbb29abc546129f4f
unknown <theytphrecopilations@gmail.com>
2023-02-13 09:42:46 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+//swiper script
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5.2,
+    spaceBetween: 30,
+    //freeMode: true,
+    //centeredSlides: false,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
